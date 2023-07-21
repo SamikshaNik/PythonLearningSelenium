91639e35f15655810fd20c49371eef4180bd0c24
samiksha Nikam <samikshakishor98@gmail.com>
2023-07-21 13:42:17 +0530

    Python Selenium Code added

---
+++ pythonProjectSelenium/day1/FirstTestCase.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+import os
+demo = os.path.exists("../Driver/chromedriver.exe")
+# driver = webdriver.chrome("../Driver/chromedriver.exe")
+# in above line we have created an object for chrome class which is present in webdriver module
+print(demo)
+# driver = webdriver.chrome(r"../Driver/chromedriver.exe")
+driver = webdriver.Chrome("../Driver/chromedriver.exe")
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+driver.implicitly_wait(10)
+driver.find_element_by_name("username").send_keys("Admin")
+driver.implicitly_wait(10)
+driver.find_element_by_name("password").send_keys("admin123")
+driver.find_element_by_xpath('button[@type="submit"]').click()
+
+
